04f9ba02aec18accf087e8e7f1395d454e2f7d52
Andrei Gheorghe <andrei.gheorghe@reevotech.com>
2023-03-14 02:45:05 +0200

    spotlight

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MDAxMg/spectral.js?branch_slug=main&token=0f58acb3-f498-4c21-873b-bb922871766f";
-export default { extends: ruleset };
